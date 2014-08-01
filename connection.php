061413b27b082d75c7e64d05d1cd8e79837b8f63
unknown <kipatel@CI0000002050750.rim.net>
2014-08-01 09:50:01 +0100

    stable

--- connection.php
+++ connection.php
@@ -2,7 +2,7 @@
 
 	$host = "localhost"; 
 	$username="root"; 
-	$password="ftstesting"; 
+	$password=""; 
 	$database="carey_car"; 
 
 	$con = mysqli_connect($host, $username, $password, $database) or die("Could Not Connect"); 
