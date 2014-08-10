a8d3dc7a25a1325f1c2264c82148fb5375f935c4
Kiran Patel <kiran_patel94@hotmail.com>
2014-08-10 18:46:43 +0100

    updated objectives

--- connection.php
+++ connection.php
@@ -6,7 +6,6 @@
 	$password=""; 
 	$database="carey_car"; 
 
-	
 
 	//Make connection and set up for being included in other files. 
 	$con = mysqli_connect($host, $username, $password) or die("Could Not Connect"); 
