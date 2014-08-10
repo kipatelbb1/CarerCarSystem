290de1fca05656226ac52effc2c62aacde696fb3
Kiran Patel <kiran_patel94@hotmail.com>
2014-08-10 17:51:23 +0100

    removed test file and commented what needs to be done next

--- scanApp/mainApp.py
+++ scanApp/mainApp.py
@@ -8,6 +8,8 @@ db = scanDB()
 #Connect to the MYSQL database. 
 con = db.connect("127.0.0.1", "root", "", "carey_car", 3306)
 
+##APPLICATION LOOP (RESET VARIABLES OR THEY MAY DO IT THEMSELVES.)
+
 #Execute Query and store 2D array in rows. 
 rows = db.getRequests(con, "SELECT r.requestid, r.date_request, r.PTime, r.PLoc, r.Duration, r.Veh_Type, r.Cost_Center, r.GL_Code, r.add_comments, t.fName, t.lName, t.email FROM request r, tester t, requestLine rl WHERE rl.requestID = r.requestID AND rl.testerID = t.testerID GROUP BY r.requestID ORDER BY r.requestID")
 
@@ -31,12 +33,19 @@ for row in rows:
 	testerEmails = db.getEmails(con, "SELECT t.email FROM tester t, requestline rl WHERE rl.testerID = t.testerID AND rl.requestID=" + row[0])
 	#For each tester related to a request ID, append thier email to the out list. 
 	for emails in testerEmails: 
-		to.append(str(emails)) 
+		#to.append(str(emails)) 
+		print emails
 	
+	#Create the body of the email with all the request details.
 	msg = mail.createBody(row, sender, to)
+	#Send the message to the out list. 
 	mail.send(sender, to, str(msg))
+	##UPDATE RECORD TO SET AS SENT. 
 
 
-print "[+] Carey Car System closing.."
+##END APPLICATION LOOP 
+print "[+] Shutting System Down.."
+#Close the connection to the SMTP server. 
 mail.close()
+#Keep window open. 
 raw_input() 
