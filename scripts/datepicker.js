dca5dbbfaad092c1e62a2e6f3665445ed1d88052
kipatelbb1 <kipatel@blackberry.com>
2014-08-11 13:16:31 +0100

    added contraint on datepicker so past date cannot be choosen

--- scripts/datepicker.js
+++ scripts/datepicker.js
@@ -1,4 +1,15 @@
 $(document).ready(function()
 {
-	$( "#datepicker" ).datepicker({dateFormat: 'dd/mm/yy'});
+	//$( "#datepicker" ).datepicker({dateFormat: 'dd/mm/yy'});
+
+
+
+	$("#datepicker").datepicker(
+    { dateFormat: 'dd/mm/yy',
+      minDate: new Date(), // Min Date to Today. 
+      
+
+    }
+   );
+
 }); 
