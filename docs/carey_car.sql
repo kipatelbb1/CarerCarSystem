dad517e94d8a740b1a79bb5fdffc21755cecf1a1
kipatelbb1 <kipatel@blackberry.com>
2014-08-15 10:59:18 +0100

    added clean sql file for installtion in new system and added documentation

--- docs/carey_car.sql
+++ docs/carey_car.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 11, 2014 at 01:08 PM
+-- Generation Time: Aug 15, 2014 at 11:57 AM
 -- Server version: 5.6.16
 -- PHP Version: 5.5.11
 
@@ -23,35 +23,21 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `request`
+-- Table structure for table `tester`
 --
 
-CREATE TABLE IF NOT EXISTS `request` (
-  `requestID` int(6) NOT NULL AUTO_INCREMENT,
-  `date_request` date NOT NULL,
-  `PTime` time NOT NULL,
-  `PLoc` varchar(100) NOT NULL,
-  `Duration` varchar(10) NOT NULL,
-  `DLocation` varchar(100) NOT NULL,
-  `Veh_Type` varchar(20) NOT NULL,
-  `Cost_Center` varchar(6) DEFAULT NULL,
-  `GL_Code` varchar(6) DEFAULT NULL,
-  `add_Comments` varchar(500) NOT NULL,
-  `status` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`requestID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COMMENT='Table contains data about requests' AUTO_INCREMENT=109 ;
-
---
--- Dumping data for table `request`
---
-
-INSERT INTO `request` (`requestID`, `date_request`, `PTime`, `PLoc`, `Duration`, `DLocation`, `Veh_Type`, `Cost_Center`, `GL_Code`, `add_Comments`, `status`) VALUES
-(103, '2014-08-12', '00:14:00', 'Slough UK', '30', 'Slough EMEA', 'executive', '313211', '650003', '', NULL),
-(104, '2014-08-21', '00:10:00', 'Slough UK', '30', 'Slough EMEA', 'executive', '313211', '650003', '', NULL),
-(105, '2014-08-19', '00:10:00', 'Slough UK', '30', 'Slough EMEA', 'executive', '313211', '650003', '', 'SENT'),
-(106, '2014-08-12', '00:10:00', 'Slough UK', '30', 'Slough EMEA', 'executive', '313211', '650003', '', 'SENT'),
-(107, '2014-08-12', '00:10:00', 'Slough UK', '30', 'Slough EMEA', 'executive', '313000', '650003', '', NULL),
-(108, '2014-08-15', '00:13:30', 'Slough UK', '30', 'Slough EMEA', 'executive', '313211', '650003', '', 'SENT');
+CREATE TABLE IF NOT EXISTS `tester` (
+  `testerID` int(3) NOT NULL AUTO_INCREMENT,
+  `fName` varchar(100) NOT NULL,
+  `lName` varchar(100) NOT NULL,
+  `MobileNo` varchar(11) NOT NULL,
+  `PLocation` varchar(200) NOT NULL,
+  `DLocation` varchar(200) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `Password` varchar(100) NOT NULL,
+  `username` varchar(10) NOT NULL,
+  PRIMARY KEY (`testerID`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COMMENT='Table holds data on the Testers' AUTO_INCREMENT=3 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
