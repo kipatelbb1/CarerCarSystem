3a70029a3e586bb7c3d5d8c84de60db3c4bf81b0
Kiran Patel <kiran_patel94@hotmail.com>
2014-08-16 04:50:12 +0100

    commit

--- connection.php
+++ connection.php
@@ -3,11 +3,11 @@
 	//Connection Details. 
 	$host = "localhost"; 
 	$username="root"; 
-	$password="ftstesting"; 
+	$password="test"; 
 	$database="carey_car"; 
 
 	//Make connection and set up for being included in other files. 
-	$con = mysqli_connect($host, $username, $password, $database) or die("Could Not Connect"); 
-	//mysql_select_db($database) or die("Could not find DB");
+	$con = mysqli_connect($host, $username, $password) or die("Could Not Connect"); 
+	mysql_select_db($database) or die("Could not find DB");
 	
 ?>
